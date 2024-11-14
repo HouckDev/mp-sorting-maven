72dad3083d85bb194cde80b4bfc417374511b2e5
HouckDev <houckdeveloper@gmail.com>
2024-11-14 10:57:59 -0600

    Begin implement mergsort

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -1,12 +1,12 @@
 package edu.grinnell.csc207.sorting;
 
 import java.util.Comparator;
+import edu.grinnell.csc207.util.ArrayUtils;
 
 /**
  * Something that sorts using merge sort.
  *
- * @param <T>
- *   The types of values that are sorted.
+ * @param <T> The types of values that are sorted.
  *
  * @author Samuel A. Rebelsky
  */
@@ -28,9 +28,7 @@ public class MergeSorter<T> implements Sorter<T> {
   /**
    * Create a sorter using a particular comparator.
    *
-   * @param comparator
-   *   The order in which elements in the array should be ordered
-   *   after sorting.
+   * @param comparator The order in which elements in the array should be ordered after sorting.
    */
   public MergeSorter(Comparator<? super T> comparator) {
     this.order = comparator;
@@ -43,18 +41,51 @@ public class MergeSorter<T> implements Sorter<T> {
   /**
    * Sort an array in place using merge sort.
    *
-   * @param values
-   *   an array to sort.
+   * @param values an array to sort.
    *
-   * @post
-   *   The array has been sorted according to some order (often
-   *   one given to the constructor).
-   * @post
-   *   For all i, 0 &lt; i &lt; values.length,
-   *     order.compare(values[i-1], values[i]) &lt;= 0
+   * @post The array has been sorted according to some order (often one given to the constructor).
+   * @post For all i, 0 &lt; i &lt; values.length, order.compare(values[i-1], values[i]) &lt;= 0
    */
   @Override
   public void sort(T[] values) {
-    // STUB
+    values = split(values);
   } // sort(T[])
+
+  T[] split(T[] values) {
+    if (values.length <= 1) {
+      return values;
+    }
+    T[] valuesA = (T[]) new Object[((int) (values.length / 2))];
+    for (int i = 0; i < valuesA.length; i++) {
+      valuesA[i] = values[i];
+    }
+    T[] valuesB = (T[]) new Object[values.length - ((int) (values.length / 2))];
+
+    for (int i = valuesA.length; i < values.length; i++) {
+      valuesB[i - valuesA.length] = values[i];
+    }
+    valuesA = split(valuesA);
+    valuesB = split(valuesB);
+    T[] merged = merge(valuesA, valuesB);
+    return merged;
+  }
+
+  T[] merge(T[] valuesA, T[] valuesB) {
+    T[] merged = (T[]) new Object[valuesA.length + valuesB.length];
+    int sort = 0;
+    for (int compare = 0; compare < valuesA.length; compare++) {
+      for (int i = 0; i < valuesB.length; i++) {
+        if (order.compare(valuesA[compare], valuesB[i]) >= 0) {
+          merged[sort] = valuesB[i];
+          sort++;
+        } else {
+          merged[sort] = valuesA[compare];
+          sort++;
+          break;
+        }
+      }
+    }
+    return merged;
+
+  }
 } // class MergeSorter
